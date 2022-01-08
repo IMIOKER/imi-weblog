44f051f2014a77a92c23489f61dd15015e7a3a58
Iman <85828680+IMIOKER@users.noreply.github.com>
2022-01-08 20:10:25 +0330

    hjfj

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from.views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Home, name='home'),
@@ -9,3 +12,6 @@ urlpatterns = [
     path('det/<id>', Detail, name='det'),
     path('es', Es, name='es')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
